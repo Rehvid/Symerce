f345e7caa90551e004a5a9212413cd5167e90b3f
Dawid <dawid.h@soffee.pl>
2025-03-15 09:03:57 +0100

    add DTO for registration

---
+++ app/src/Dto/RegistrationDto.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Dto;
+
+use App\Validator\RepeatPassword as CustomAssertRepeatPassword;
+use App\Validator\StrongPassword as CustomAssertStrongPassword;
+use App\Validator\UniqueEmail as CustomAssertUniqueEmail;
+use Symfony\Component\Validator\Constraints as Assert;
+
+final readonly class RegistrationDto
+{
+    public function __construct(
+        #[Assert\NotBlank] #[Assert\Email] #[CustomAssertUniqueEmail]  public string $email,
+        #[Assert\NotBlank] #[CustomAssertStrongPassword] public string $password,
+        #[Assert\NotBlank] #[CustomAssertRepeatPassword] public string $passwordConfirmation,
+    ) {}
+}
