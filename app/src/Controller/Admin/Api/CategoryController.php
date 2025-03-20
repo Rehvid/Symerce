001b4070f4abf7debd630de47b975b37439122f4
Dawid <dawid.h@soffee.pl>
2025-03-20 19:57:06 +0100

    Add api category controller

---
+++ app/src/Controller/Admin/Api/CategoryController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin\Api;
+
+use App\Service\PaginatedListService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api/category', name: 'api_category_')]
+class CategoryController extends AbstractController
+{
+    #[Route('/list', name: 'list', methods: ['GET'])]
+    public function getList(Request $request, PaginatedListService $paginatedListService): JsonResponse
+    {
+        return $this->json($paginatedListService->getListResponse($request));
+    }
+}
