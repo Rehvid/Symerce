0c148212f2ea1557e4d191fb009f29d68c5f9f12
Dawid <dawid.h@soffee.pl>
2025-03-23 14:24:48 +0100

    Add component card

---
+++ app/assets/admin/components/Card.jsx
@@ -0,0 +1,9 @@
+function Card ({children, additionalClasses = ''}) {
+    return (
+        <div className={`rounded-xl border border-gray-200 bg-white p-6 ${additionalClasses}`}>
+            {children}
+        </div>
+    )
+}
+
+export default Card;
