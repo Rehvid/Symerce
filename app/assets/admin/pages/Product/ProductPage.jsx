ed0856e4bf4423975f7ca67c0497c817b3ff6a4e
Dawid <dawid.h@soffee.pl>
2025-03-23 11:54:41 +0100

    Clean product page to default one

--- app/assets/admin/pages/Product/ProductPage.jsx
+++ app/assets/admin/pages/Product/ProductPage.jsx
@@ -1,20 +1,6 @@
-import Breadcrumb from '../../components/Navigation/Breadcrumb';
-import PageHeader from '../../components/Layout/PageHeader';
 
 function ProductPage() {
-    return (
-        <>
-            <PageHeader title={'Products'}>
-                <button
-                    className="cursor-pointer w-full bg-indigo-500 text-white text-sm font-bold py-3 px-4 rounded-md hover:bg-indigo-600 transition duration-300"
-                    type="submit"
-                >
-                    New Product
-                </button>
-            </PageHeader>
-            <Breadcrumb />
-        </>
-    );
+    return <></>;
 }
 
 export default ProductPage;
