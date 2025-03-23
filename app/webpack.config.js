5cdbd3404f0ad4ba829b72c35bd23a586bb77d05
Dawid <dawid.h@soffee.pl>
2025-03-23 11:47:42 +0100

    Add react paginate and svg packages

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,9 +1,14 @@
 const Encore = require('@symfony/webpack-encore');
 const Dotenv = require('dotenv-webpack');
+const path = require('path');
 
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
+
+Encore.configureLoaderRule('images', (loaderRule) => {
+    loaderRule.exclude = /\.svg$/;
+});
 
 Encore
     .setOutputPath('public/build/')
@@ -23,6 +28,21 @@ Encore
     .enableReactPreset()
     .enablePostCssLoader()
     .addPlugin(new Dotenv())
+
+    .addRule({
+        test: /\.svg$/,
+        use: [
+            {
+                loader: '@svgr/webpack',
+            },
+        ],
+    })
+
+    // .addRule({
+    //     test: /\.svg$/,
+    //     issuer: /\.[jt]sx?$/,
+    //     use: ['@svgr/webpack', 'file-loader'],
+    // })
 ;
 
 module.exports = Encore.getWebpackConfig();
