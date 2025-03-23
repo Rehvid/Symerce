738a63b130567970db4297038a5b25442f9b413b
Dawid <dawid.h@soffee.pl>
2025-03-23 11:49:36 +0100

    Add universal components for button and link

---
+++ app/assets/admin/components/Common/AppButton.jsx
@@ -0,0 +1,22 @@
+function AppButton({ children, id, variant, additionalClasses = 'bg-primary', type = 'button', ...props }) {
+    const variants = {
+        primary: 'bg-primary text-white hover:bg-primary-stronger',
+        secondary: 'bg-white hover:bg-gray-100 text-gray-700 border border-gray-300',
+        link: 'bg-white text-gray-700 text-sm hover:bg-gray-100 hover:text-gray-900',
+    };
+
+    return (
+        <button
+            id={id}
+            type={type}
+            className={`transition-all rounded-full cursor-pointer ${
+                variant ? variants[variant] : ''
+            } ${additionalClasses}`}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+}
+
+export default AppButton;
