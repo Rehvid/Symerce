c1eebcc26df20356c19f6ac64db1353d312792eb
Dawid <dawid.h@soffee.pl>
2025-03-26 07:21:25 +0100

    Add interface for responses from server

---
+++ app/src/Dto/Response/Category/CategoryFormResponseDTO.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Dto\Response\Category;
+
+use App\Dto\Response\ResponseInterfaceData;
+
+final readonly class CategoryFormResponseDTO implements ResponseInterfaceData
+{
+    private function __construct(
+       public array $tree,
+       public ?string $name,
+       public ?int $parentCategoryId = null,
+       public ?string $description = null,
+       public bool $isActive,
+    ){}
+
+
+    public static function fromArray(array $data): ResponseInterfaceData
+    {
+        return new self(
+          tree: $data['tree'] ?? [],
+          name: $data['name'] ?? null,
+          parentCategoryId: $data['parentId'] ?? null,
+          description: $data['description'] ?? null,
+          isActive: $data['isActive'] ?? false,
+        );
+    }
+}
