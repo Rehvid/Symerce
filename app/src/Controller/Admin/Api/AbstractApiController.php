fd835ecd20d8689e25bfd9df8126a11edbe63c68
Dawid <dawid.h@soffee.pl>
2025-03-26 13:23:50 +0100

    Adjust abstract api controller

--- app/src/Controller/Admin/Api/AbstractApiController.php
+++ app/src/Controller/Admin/Api/AbstractApiController.php
@@ -5,14 +5,18 @@ declare(strict_types=1);
 namespace App\Controller\Admin\Api;
 
 use App\Dto\Response\ApiResponse;
+use App\Service\DataPersister\Manager\DataPersisterManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class AbstractApiController extends AbstractController
 {
+    public function __construct(protected readonly DataPersisterManager $dataPersisterManager)
+    {}
+
     protected function prepareJsonResponse(
-        mixed $data,
+        mixed $data = [],
         ?array $meta = null,
         ?array $errors = null,
         int $statusCode = Response::HTTP_OK
