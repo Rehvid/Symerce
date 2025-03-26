baa2a6ae9af73549297573026ebe294d9b34b7a9
Dawid <dawid.h@soffee.pl>
2025-03-26 06:09:16 +0100

    Add slugger service

---
+++ app/src/Service/SluggerService.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Service;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Ramsey\Uuid\Guid\Guid;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+
+final readonly class SluggerService
+{
+    private const string LIMITER = '-';
+    private const int MAX_ATTEMPTS = 3;
+
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private AsciiSlugger $slugger = new AsciiSlugger()
+    ) {
+    }
+
+    public function slugify(string $value): string
+    {
+        return $this->slugger->slug($value)->lower()->toString();
+    }
+
+    public function slugifyUnique(string $value, string $entityClass, string $fieldName): string
+    {
+        $slug = $this->slugify($value);
+
+        if (!$this->findExistingSlugs($slug, $entityClass, $fieldName)) {
+            return $slug;
+        }
+
+        return $this->tryToGenerateUniqueSlug($slug, $entityClass, $fieldName);
+    }
+
+    private function findExistingSlugs(string $value, string $entityClass, string $fieldName): bool
+    {
+        $count = $this->entityManager
+            ->getRepository($entityClass)
+            ->createQueryBuilder('c')
+            ->select('COUNT(c.' . $fieldName . ')')
+            ->where('c.' . $fieldName . ' = :value')
+            ->setParameter('value', $value)
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        return $count > 0;
+    }
+
+    private function tryToGenerateUniqueSlug(string $slug, string $entityClass, string $fieldName): string
+    {
+        $guid = Guid::uuid4()->toString();
+        $newSlug = $slug . self::LIMITER . $guid;
+
+        if (!$this->findExistingSlugs($newSlug, $entityClass, $fieldName)) {
+            return $newSlug;
+        }
+
+        $attempts = 1;
+        while ($this->findExistingSlugs($newSlug, $entityClass, $fieldName) && $attempts <= self::MAX_ATTEMPTS) {
+            $attempts++;
+            $newSlug = $slug . self::LIMITER . $attempts . self::LIMITER . Guid::uuid4()->toString();
+        }
+
+        if ($attempts === self::MAX_ATTEMPTS) {
+            throw new \LogicException('Too many attempts to generate a unique slug');
+        }
+
+        return $newSlug;
+    }
+}
