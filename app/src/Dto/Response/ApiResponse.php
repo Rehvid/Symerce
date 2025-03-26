c98baf7d8d67222acd1bdb143753b453d8baa107
Dawid <dawid.h@soffee.pl>
2025-03-26 07:20:44 +0100

    Create standard api response

---
+++ app/src/Dto/Response/ApiResponse.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Dto\Response;
+
+final readonly class ApiResponse
+{
+    public function __construct(
+        public mixed $data,
+        public ?array $meta = null,
+        public ?array $errors = null,
+    ) {}
+
+    public function toArray(): array
+    {
+        return [
+            'data' => $this->data,
+            'meta' => $this->meta,
+            'errors' => $this->errors,
+        ];
+    }
+}
