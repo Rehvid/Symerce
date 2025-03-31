559ba0c2b0c22a8088d61cbec15ff28c5ede2d0c
Dawid <dawid.h@soffee.pl>
2025-03-31 16:42:45 +0200

    Add forbidden and not found page

---
+++ app/assets/admin/pages/NotFoundPage.jsx
@@ -0,0 +1,17 @@
+import AppButton from "@/admin/components/Common/AppButton";
+import {useNavigate} from "react-router-dom";
+
+const NotFoundPage = () => {
+    const navigate = useNavigate();
+    return (
+        <div className="flex flex-col min-h-screen justify-center items-center gap-6">
+            <h1 className="text-7xl font-medium uppercase mb-4">404</h1>
+            <div className="text-3xl text-gray-700 mb-4">Przepraszamy, ale strona, której szukasz, nie istnieje lub została przeniesiona.</div>
+            <AppButton onClick={()=> navigate("/admin/dashboard")} variant="primary" additionalClasses="py-3 px-5 uppercase">
+                Wróć na stronę główną
+            </AppButton>
+        </div>
+    )
+}
+
+export default NotFoundPage;
