e5d67cc7a3a2f91af163c351cd55516bc79d62cb
Dawid <dawid.h@soffee.pl>
2025-04-01 08:30:14 +0200

    Remove unnecessary component for route

--- app/assets/admin/routes/authorizationRoutes.js
+++ app/assets/admin/routes/authorizationRoutes.js
@@ -1,21 +1,16 @@
 import {Route} from "react-router-dom";
 import RegisterPage from "@/admin/pages/Auth/RegisterPage";
 import LoginPage from "@/admin/pages/Auth/LoginPage";
-import PublicRoute from "@/admin/components/Route/PublicRoute";
 
 const authorizationRoutes = (
     <>
         <Route
             path="/admin/register"
-            element={
-                <PublicRoute component={<RegisterPage />} redirectOnAuthSuccess="/admin/dashboard" />
-            }
+            element={<RegisterPage />}
         />
         <Route
             path="/admin/login"
-            element={
-                <PublicRoute component={<LoginPage />} redirectOnAuthSuccess="/admin/dashboard" />
-            }
+            element={<LoginPage />}
         />
     </>
 );
