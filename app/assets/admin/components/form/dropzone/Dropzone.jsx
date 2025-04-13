f7fb558bd3726f4365d78c2c8abeb18072809c98
Dawid <dawid.h@soffee.pl>
2025-04-13 13:34:22 +0200

    Move logic from dropzone component to hook

--- app/assets/admin/components/form/dropzone/Dropzone.jsx
+++ app/assets/admin/components/form/dropzone/Dropzone.jsx
@@ -1,138 +1,33 @@
 import { useDropzone } from 'react-dropzone';
-import React, { useEffect, useState } from 'react';
-import { convertFileToBase64 } from '@/admin/utils/helper';
-import { useApi } from '@/admin/hooks/useApi';
-import { createApiConfig } from '@/shared/api/ApiConfig';
-import { HTTP_METHODS } from '@/admin/constants/httpConstants';
-import DropzoneContent from '@/admin/components/form/dropzone/DropzoneContent';
+import React from 'react';
+import PhotosIcon from '@/images/icons/photos.svg';
 
-const Dropzone = ({
-    setValue,
-    onChange,
-    renderModal,
-    dropzoneThumbnailAdditionalActions = null,
-    value = [],
-    maxFiles = 1,
-    maxSize = 5,
-    accept = ['image/jpeg', 'image/png'],
-}) => {
-    const [errors, setErrors] = useState({});
-    const { handleApiRequest } = useApi();
-
-    const onDrop = (acceptedFiles) => {
-        setValue((prevFiles) => {
-            const currentFiles = [...prevFiles];
-
-            if (currentFiles.length + acceptedFiles.length > maxFiles) {
-                setErrors({
-                    message: `Możesz przesłać maksymalnie ${maxFiles} plik${maxFiles > 1 ? 'ów' : ''}.`,
-                });
-                return prevFiles;
-            }
-
-            const filteredFiles = acceptedFiles.filter((file) => {
-                const isAcceptedType = accept.includes(file.type);
-                const isAcceptedSize = file.size <= maxSize * 1024 * 1024;
-
-                if (!isAcceptedType) {
-                    setErrors({
-                        message: `Nieprawidłowy format pliku. Dozwolone: ${accept.join(', ')}`,
-                    });
-                    return false;
-                }
-
-                if (!isAcceptedSize) {
-                    const sizeMB = maxSize.toFixed(2);
-                    setErrors({
-                        message: `Plik jest za duży. Maksymalny rozmiar to ${sizeMB} MB`,
-                    });
-                    return false;
-                }
-
-                return true;
-            });
-
-            if (filteredFiles.length === 0) return prevFiles;
-
-            const withPreview = filteredFiles.map((file) =>
-                Object.assign(file, {
-                    preview: URL.createObjectURL(file),
-                }),
-            );
-
-            const updated = [...currentFiles, ...withPreview];
-            setErrors({});
-            return updated;
-        });
-    };
-
-    const { getRootProps, getInputProps } = useDropzone({
-        onDrop,
-    });
-
-    useEffect(() => {
-        const copyFiles = [...value];
-        const newFiles = copyFiles.map((file) => {
-            if (file.preview) {
-                URL.revokeObjectURL(file.preview);
-            }
-
-            return {
-                id: file.id,
-                name: file.originalName,
-                preview: file.path,
-            };
-        });
-
-        setValue(newFiles);
-    }, []);
-
-    useEffect(() => {
-        const handleFilesChange = async () => {
-            const copyFiles = [...value];
-            const base64Files = await Promise.all(
-                copyFiles.map(async (file) => {
-                    const base64 = await convertFileToBase64(file);
-                    return {
-                        size: file.size,
-                        type: file.type,
-                        name: file.name,
-                        content: base64,
-                    };
-                }),
-            );
-            onChange?.(base64Files);
-        };
-
-        handleFilesChange();
-    }, [value]);
-
-    const removeFile = (file) => {
-        const updatedFiles = value.filter((item) => item !== file);
-
-        if (file.id) {
-            handleApiRequest(createApiConfig(`admin/files/${file.id}`, HTTP_METHODS.DELETE), {
-                onSuccess: () => {
-                    setValue(updatedFiles);
-                },
-            });
-        } else {
-            setValue(updatedFiles);
-        }
-
-        setErrors({});
+const Dropzone = ({ onDrop, errors, variant = 'sideColumn', containerClasses = '', children }) => {
+    const { getRootProps, getInputProps } = useDropzone({ onDrop });
+    const variants = {
+        sideColumn: `dropzone max-w-full  min-h-[130px] rounded-md `,
+        avatar: 'rounded-full h-40 w-40',
     };
 
     return (
-        <DropzoneContent
-            getRootProps={getRootProps}
-            getInputProps={getInputProps}
-            errors={errors}
-            files={value}
-            renderModal={renderModal}
-            removeFile={removeFile}
-            dropzoneThumbnailAdditionalActions={dropzoneThumbnailAdditionalActions}
-        />
+        <section className={`container ${containerClasses}`}>
+            <div
+                {...getRootProps({
+                    className: `${variants[variant] ?? ''} flex flex-col gap-2 px-4 py-2 justify-center items-center border border-dashed ${errors && errors.message ? 'border-red-500 hover:border-red-700 hover:border-2' : 'border-gray-300 hover:border-primary-stronger'} hover:scale-[105%] transition-all cursor-pointer`,
+                })}
+            >
+                <div>
+                    <PhotosIcon />
+                </div>
+                <input {...getInputProps()} />
+                <p className="text-center mt-1 text-xs">
+                    <span className="text-gray-800">Drop your image here, or </span>
+                    <span className="text-primary">Click to browse</span>
+                </p>
+            </div>
+            {errors && errors.message && <p className="mt-2 mb-2 pl-2 text-sm text-red-600">{errors.message}</p>}
+            {children}
+        </section>
     );
 };
 export default Dropzone;
---
+++ app/assets/admin/components/form/dropzone/DropzonePreviewActions.jsx
@@ -0,0 +1,28 @@
+import EyeIcon from '@/images/icons/eye.svg';
+import TrashIcon from '@/images/icons/trash.svg';
+import React from 'react';
+import { useModal } from '@/admin/hooks/useModal';
+
+const DropzonePreviewActions = ({ renderModal, removeFile, file }) => {
+    const { openModal } = useModal();
+
+    return (
+        <>
+            {renderModal && (
+                <span className="block cursor-pointer" onClick={() => openModal(renderModal(file))}>
+                    <EyeIcon className="text-white scale-125%" />
+                </span>
+            )}
+            <span
+                className="block cursor-pointer"
+                onClick={() => {
+                    removeFile(file);
+                }}
+            >
+                <TrashIcon className="text-white scale-125%" />
+            </span>
+        </>
+    );
+};
+
+export default DropzonePreviewActions;
