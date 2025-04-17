fbb457e566f79962c92b3721a6cb205d0e7dffb5
Dawid <dawid.h@soffee.pl>
2025-04-17 15:38:26 +0200

    Add strict mode and remove unused js code

--- app/assets/admin/index.js
+++ app/assets/admin/index.js
@@ -3,7 +3,12 @@ import '../styles/app.css';
 import 'quill/dist/quill.core.css';
 import 'quill/dist/quill.snow.css';
 import App from './app';
+import { StrictMode } from 'react';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
