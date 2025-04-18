af2298810e2a2deed65397e8b9b9b563233b212f
Dawid <dawid.h@soffee.pl>
2025-04-18 06:41:42 +0200

    Add confirm or cancel button for delete modal

--- app/assets/admin/components/table/Partials/TableRow/TableRowDeleteAction.jsx
+++ app/assets/admin/components/table/Partials/TableRow/TableRowDeleteAction.jsx
@@ -1,9 +1,43 @@
-import TrashIcon from '../../../../../images/icons/trash.svg';
-import AppButton from '../../../common/AppButton';
+import TrashIcon from '@/images/icons/trash.svg';
+import AppButton from '@/admin/components/common/AppButton';
+import { useModal } from '@/admin/hooks/useModal';
+import ModalHeader from '@/admin/components/modal/ModalHeader';
+import ModalBody from '@/admin/components/modal/ModalBody';
+import React from 'react';
 
 const TableRowDeleteAction = ({ onClick }) => {
+    const { openModal, closeModal } = useModal();
+
+    const handleClick = () => {
+      const title = (
+        <div className="flex flex-col items-center gap-3 px-6">
+          <TrashIcon className="scale-[125%] text-gray-700" />
+          <span>Czy na pewno chcesz usunąć ten element?</span>
+        </div>
+      );
+
+      const confirmClick = () => {
+        onClick();
+        closeModal();
+      }
+
+      openModal(
+        <>
+          <ModalHeader title={title} />
+          <ModalBody>
+            <div className="flex justify-between">
+              <AppButton variant="primary" additionalClasses="px-4 py-2.5 font-bold" onClick={() => confirmClick()}>Tak</AppButton>
+              <AppButton variant="secondary" additionalClasses="px-4 py-2.5 font-bold" onClick={closeModal} >Nie</AppButton>
+            </div>
+          </ModalBody>
+        </>
+      )
+    }
+
+
+
     return (
-        <AppButton onClick={onClick} additionalClasses="text-gray-500">
+        <AppButton onClick={handleClick} additionalClasses="text-gray-500">
             <TrashIcon />
         </AppButton>
     );
