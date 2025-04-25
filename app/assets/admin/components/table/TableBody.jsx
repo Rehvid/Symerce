1c5a022ca5dc8ad2b72372b30764a856bf56b547
Dawid <dawid.h@soffee.pl>
2025-04-25 17:46:45 +0200

    Add row for empty items

--- app/assets/admin/components/table/TableBody.jsx
+++ app/assets/admin/components/table/TableBody.jsx
@@ -85,36 +85,44 @@ const TableBody = ({ data, useDraggable, draggableCallback }) => {
 
     return (
         <tbody>
-            {items.map((row, rowIndex) => {
+        {items.length > 0 ? (
+          items.map((row, rowIndex) => {
               const isDragging = draggedItemIndex === rowIndex;
               const rowClasses = `border-b border-gray-100 last:border-b-0 transition-all duration-150 ease-in-out hover:cursor-grab ${
                 isDragging ? 'opacity-50 scale-[1.01] bg-slate-100' : ''
               }`;
-                return (
-                  <Fragment  key={`row-wrapper-${rowIndex}`} >
+              return (
+                <Fragment  key={`row-wrapper-${rowIndex}`} >
                   {useDraggable && dragOverItemIndex === rowIndex && renderPlaceholder(rowIndex)}
 
                   {useDraggable ? (
-                  <tr
-                    onDragStart={(e) => dragStart(e, rowIndex)}
-                    onDragEnter={(e) => dragEnter(e, rowIndex)}
-                    onDragOver={(e) => e.preventDefault()}
-                    onDragEnd={drop}
-                    draggable
-                    key={rowIndex}
-                    id={rowIndex}
-                    className={rowClasses}
-                  >
-                    {renderCells(row)}
-                  </tr>
+                    <tr
+                      onDragStart={(e) => dragStart(e, rowIndex)}
+                      onDragEnter={(e) => dragEnter(e, rowIndex)}
+                      onDragOver={(e) => e.preventDefault()}
+                      onDragEnd={drop}
+                      draggable
+                      key={rowIndex}
+                      id={rowIndex}
+                      className={rowClasses}
+                    >
+                      {renderCells(row)}
+                    </tr>
                   ) : (
-                  <tr key={rowIndex} id={rowIndex} className="border-b border-gray-100 last:border-b-0">
-                    {renderCells(row)}
-                  </tr>
+                    <tr key={rowIndex} id={rowIndex} className="border-b border-gray-100 last:border-b-0">
+                      {renderCells(row)}
+                    </tr>
                   )}
                 </Fragment>
-                )
-            })}
+              )
+            })
+        ) : (
+          <tr>
+            <td colSpan="100%" className="text-center p-5 text-gray-500 text-md font-bold">
+              Brak danych
+            </td>
+          </tr>
+        )}
         </tbody>
     );
 };
