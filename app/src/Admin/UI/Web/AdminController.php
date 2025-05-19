18b7a98e7aef62d94dc8b246b695c20549cc570d
Dawid <dawid.h@soffee.pl>
2025-05-19 12:48:40 +0200

    Move admin controller from old structrue a new one

--- app/src/Admin/UI/Web/AdminController.php
+++ app/src/Admin/UI/Web/AdminController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Controller\Admin;
+namespace App\Admin\UI\Web;
 
 use App\Service\DataProvider\AppConfigProvider;
 use App\Service\DataProvider\ReactDataProvider;
@@ -12,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class AdminController extends AbstractController
+final class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
     #[Route('/admin/{reactRoute}', name: 'app_admin_react', requirements: ['reactRoute' => '.*'])]
