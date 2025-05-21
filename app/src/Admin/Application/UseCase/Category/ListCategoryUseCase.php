929b7973164edd3344dd8ec826b590101f47e4e3
Dawid <dawid.h@soffee.pl>
2025-05-21 05:06:46 +0200

    Change pagination logic from deprecated and old code to new one for category

--- app/src/Admin/Application/UseCase/Category/ListCategoryUseCase.php
+++ app/src/Admin/Application/UseCase/Category/ListCategoryUseCase.php
@@ -5,28 +5,28 @@ declare(strict_types=1);
 namespace App\Admin\Application\UseCase\Category;
 
 use App\Admin\Application\Assembler\CategoryAssembler;
-use App\Admin\Domain\Repository\CategoryRepositoryInterface;
-use App\Service\Pagination\PaginationService;
-use App\Service\Response\ApiResponse;
+use App\Admin\Application\Search\Category\CategorySearchService;
+use App\Shared\Application\DTO\Response\ApiResponse;
 use App\Shared\Application\UseCases\Base\ListUseCaseInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 final readonly class ListCategoryUseCase implements ListUseCaseInterface
 {
     public function __construct(
-        private CategoryRepositoryInterface $repository,
         private CategoryAssembler $assembler,
-        private PaginationService $paginationService,
+        private CategorySearchService $searchService,
     ) {
     }
 
     public function execute(Request $request): ApiResponse
     {
-        $paginationResponse = $this->paginationService->buildPaginationResponse($request, $this->repository);
+        $paginationResult = $this->searchService->search(
+            $this->searchService->buildSearchCriteria($request)
+        );
 
         return new ApiResponse(
-            data: $this->assembler->toListResponse($paginationResponse->data),
-            meta: $paginationResponse->paginationMeta->toArray(),
+            data: $this->assembler->toListResponse($paginationResult->items),
+            meta: $paginationResult->paginationMeta->toArray(),
         );
     }
 }
--- app/src/Admin/Infrastructure/Repository/CategoryDoctrineRepository.php
+++ app/src/Admin/Infrastructure/Repository/CategoryDoctrineRepository.php
@@ -4,24 +4,12 @@ declare(strict_types=1);
 
 namespace App\Admin\Infrastructure\Repository;
 
-use App\Admin\Domain\Enums\OrderByField;
 use App\Admin\Domain\Repository\CategoryRepositoryInterface;
 use App\Entity\Category;
-use App\Factory\FilterBuilderFactory;
-use App\Repository\Base\AbstractRepository;
-use App\Service\Pagination\PaginationFilters;
-use App\Shared\Domain\Enums\DirectionType;
-use Doctrine\ORM\QueryBuilder;
-use Doctrine\Persistence\ManagerRegistry;
+use App\Shared\Infrastructure\Repository\AbstractCriteriaRepository;
 
-class CategoryDoctrineRepository extends AbstractRepository implements CategoryRepositoryInterface
+class CategoryDoctrineRepository extends AbstractCriteriaRepository implements CategoryRepositoryInterface
 {
-    public function __construct(
-        ManagerRegistry $registry,
-        private readonly FilterBuilderFactory $filterBuilderFactory,
-    ) {
-        parent::__construct($registry);
-    }
 
     protected function getEntityClass(): string
     {
@@ -31,20 +19,6 @@ class CategoryDoctrineRepository extends AbstractRepository implements CategoryR
     protected function getAlias(): string
     {
         return 'c';
-    }
-
-    protected function configureQueryForPagination(QueryBuilder $queryBuilder, PaginationFilters $paginationFilters): QueryBuilder
-    {
-        $alias = $this->getAlias();
-
-        $filterBuilder = $this->filterBuilderFactory->create($queryBuilder, $paginationFilters, $alias);
-        $filterBuilder->applyIsActive();
-
-        if ($paginationFilters->hasOrderBy()) {
-            return $queryBuilder;
-        }
-
-        return $queryBuilder->orderBy("$alias.".OrderByField::ORDER->value, DirectionType::ASC->value);
     }
 
     public function getMaxOrder(): int
