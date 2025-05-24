77bd8a0e32368bc9768fa75ae5388763e37316cb
Dawid <dawid.h@soffee.pl>
2025-05-24 17:16:31 +0200

    Adjust font sizes

--- app/assets/admin/components/common/Heading.jsx
+++ app/assets/admin/components/common/Heading.jsx
@@ -1,11 +1,11 @@
 const Heading = ({ level = 'h1', children, additionalClassNames = '' }) => {
     const headingStyles = {
-        h1: 'text-3xl lg:text-4xl font-bold leading-tight',
-        h2: 'text-2xl lg:text-3xl font-semibold leading-snug',
-        h3: 'text-xl font-semibold leading-snug',
-        h4: 'text-lg font-medium leading-snug',
-        h5: 'text-base font-medium leading-normal',
-        h6: 'text-sm font-medium leading-normal uppercase',
+        h1: 'text-xl font-semibold',
+        h2: 'text-lg font-semibold',
+        h3: 'text-base font-medium',
+        h4: 'text-sm font-medium',
+        h5: 'text-xs font-medium uppercase tracking-wide text-gray-600',
+        h6: 'text-xs font-medium text-gray-500 uppercase tracking-wider',
     };
     const HeadingTag = level;
 
